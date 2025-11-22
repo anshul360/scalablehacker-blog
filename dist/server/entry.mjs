a6dc7a654e0a950edbf060f281df02576bb244b8
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2025-11-22 18:42:54 +0530

    pagefind

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DIzNHMby.mjs';
-import { manifest } from './manifest_B8EraII3.mjs';
+import { manifest } from './manifest_Uny5QLUb.mjs';
 
 const serverIslandMap = new Map();;
 
