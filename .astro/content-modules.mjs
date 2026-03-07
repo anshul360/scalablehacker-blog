21ddd9ce819449faeaf4aeecf435a0b59a91f779
Anshul Kumar <got.trapped.in.the.matrix@gmail.com>
2026-03-07 17:31:03 +0530

    rename

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -4,5 +4,6 @@ export default new Map([
 ["src/content/post/assignment2/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fassignment2%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/post/assignment1/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fassignment1%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/post/assigment4/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fassigment4%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/post/assignment3/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fassignment3%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/post/assignment3/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fassignment3%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/post/assignment4/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fassignment4%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
